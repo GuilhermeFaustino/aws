ac6db11d00b226b5b3168d3d11cbc171ff9c86fd
Guilherme Mendes <35610614+GuilhermeFaustino@users.noreply.github.com>
2023-10-09 09:25:17 -0300

    arrays
    Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print(). O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc). O segundo array deve conter os anos de nascimento de familiares e amigos seus. Lembre-se de usar nomes descritivos para nomear cada variável, e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).

---
+++ arrays.py
@@ -0,0 +1,10 @@
+
+produtos_loja = ["Maçãs", "Camisetas", "Parafusos", "Livros", "Guitarras"]
+precos_produtos = [2.99, 12.99, 0.25, 19.99, 399.99]
+
+# Array de anos de nascimento de familiares e amigos
+anos_nascimento = [1990, 1985, 1992, 1980, 2000]
+
+print(produtos_loja)
+print(precos_produtos)
+print(anos_nascimento)
